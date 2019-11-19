c2d55cb9a4eea3aea2cd1b831d820ce9faf5646e
Enno Welbers <enno.welbers@googlemail.com>
2019-11-19 21:40:27 +0100

    built the class generator (no validation yet)

---
+++ class/renderer.js
@@ -0,0 +1,116 @@
+"use strict";
+
+class ClassRenderer {
+	
+	
+	constructor(canvas) {
+		this.canvas = canvas;
+		if(typeof(FontFace) != 'undefined') {
+			this.font = new FontFace('terminator','url(Terminat.woff)');
+			this.fontLoaded=false;
+			this.font.load().then((function(font) {
+				document.fonts.add(font);
+				this.fontLoaded=true;
+				this.canvas.dispatchEvent(new CustomEvent('fontLoaded'));
+			}).bind(this));			
+		} else {
+			this.fontLoaded=true;
+		}
+		this.context = canvas.getContext('2d');
+		
+		this.backgrounds = {};
+		this.backgrounds['red'] = $("#background_red")[0];
+		this.backgrounds['green'] = $("#background_green")[0];
+		this.backgrounds['blue'] = $("#background_blue")[0];
+		this.backgrounds['yellow'] = $("#background_yellow")[0];
+		this.backgrounds['purple'] = $("#background_purple")[0];
+		this.artwork = $("#artwork")[0];
+		this.templates = {};
+		this.templates['normal']=$("#template_normal")[0];
+		this.templates['mech']=$("#template_mech")[0];
+		
+		//we have to fetch all skills into an ordered array.
+		this.skills = {};
+		
+		$("div.skills img").each((function(idx,obj){
+			var skillname=$(obj).data("name");
+			var skillgroup=$(obj).parent().data("class");
+			var image = obj;
+			if(this.skills[skillgroup] == null) {
+				this.skills[skillgroup]= { };			
+			}
+			this.skills[skillgroup][skillname]=image;
+		}).bind(this));
+		
+		this.Positions = {};
+		this.Positions['normal'] = [
+			{x:100,y:180},
+			{x:230,y:150},
+			{x:360,y:178},
+			{x:500,y:200},
+			{x:460,y:63},
+			{x:590,y:100},
+			{x:690,y:165},
+		];
+		
+		this.Positions['mech']  = [
+			{x:100,y:165},
+			{x:255,y:150},
+			{x:395,y:155},
+			{x:590,y:195},
+			{x:545,y:68},
+			{x:695,y:125},
+		];
+		
+		
+		this.ActiveBackground='blue';
+		this.ActiveTemplate='normal';
+		this.ActiveSkills=[];
+	}
+			
+	drawRegularSkill(skill) {
+		if(typeof this.skills[skill.Skill[0]] == 'undefined') {
+			//the skill selection is incomplete. bail out!
+			return;
+		}
+		if(typeof this.skills[skill.Skill[0]][skill.Skill[1]] == 'undefined') {
+			//the skill selection is incomplete. bail out!
+			return;
+		}
+		var image = this.skills[skill.Skill[0]][skill.Skill[1]];
+		this.context.drawImage(image,-(image.width/2),-(image.height/2));
+	}
+	
+	drawSkill(centerX, centerY, skill) {
+		var context = this.context;
+		context.save();
+		context.translate(centerX,centerY);
+		context.scale(0.8,0.8);
+		
+		this.drawRegularSkill(skill);
+		context.restore();
+	}
+
+	
+	render() {
+		
+		var context = this.context;
+
+		context.fillStyle='white';
+		context.fillRect(0,0,canvas.width,canvas.height);
+		context.drawImage(this.backgrounds[this.ActiveBackground],0,0);
+		context.drawImage(this.artwork,0,0);
+		context.drawImage(this.templates[this.ActiveTemplate],0,0);
+		for(var i=0;i<this.ActiveSkills.length;i++) {
+			if (i < this.Positions[this.ActiveTemplate].length) {
+				this.drawSkill(this.Positions[this.ActiveTemplate][i].x,this.Positions[this.ActiveTemplate][i].y,this.ActiveSkills[i]);
+			}
+		}
+		return this.canvas.toDataURL('image/png');
+	}
+}
+
+$(window).on('load',function(){
+	window.renderer = new ClassRenderer($('#canvas')[0]);
+	$("a.download").attr('href',window.renderer.render());
+});
---
+++ class/userinput.js
@@ -0,0 +1,118 @@
+"use strict";
+
+$(window).on('load',function(){
+	window.kickingoff=false;
+	var kickoff=function(readurl=false) {
+		if(window.kickingoff) { return;}
+		window.kickingoff=true;
+		//lets create a characterset.
+		var dataset={};
+		dataset.ActiveBackground = $("select.background").val();
+		dataset.ActiveTemplate = $("select.template").val();
+		dataset.ActiveSkills = [];
+		
+		
+		$("div.skillset").each(function(idx,elem){
+			var idx = $(elem).data('idx');
+			var Skill = [
+				$("div.SKILL select.SKILLGROUP",elem).val(),
+				$("div.SKILL select.SKILL",elem).val()
+			];
+			if(Skill[0] != 'null' && Skill[1] != 'null') {
+				dataset.ActiveSkills[idx] = {Skill:Skill};				
+			}
+		});
+		
+		if(readurl) {
+			var url = new URL(window.location.href);
+			var encoded = url.searchParams.get('class');
+			if(typeof encoded !== 'undefined' && encoded !== '' && encoded !== null) {
+				encoded = atob(encoded);
+				dataset=JSON.parse(encoded);				
+			}
+		}
+
+		if(readurl) {
+			//some values have been updated. we have to reapply them to the corresponding inputs.
+			$("div.skillset").each(function(idx,elem){
+				var tmp = $(elem).data('idx');
+				if(dataset.ActiveSkills.length>tmp) {
+					$("select.SKILLGROUP",elem).val(dataset.ActiveSkills[$(elem).data('idx')].Skill[0]);
+					$("select.SKILLGROUP",elem).trigger('change');
+					$("select.SKILL",elem).val(dataset.ActiveSkills[$(elem).data('idx')].Skill[1]);
+					$("select.SKILL",elem).trigger('change');
+				}
+			});
+			//we need to fire our label updating logic.
+		}
+		Object.keys(dataset).forEach(function(key){
+			if(key == "valid" || key == "updated" || key == "validation_errors") {
+				return;
+			}
+			window.renderer[key]=dataset[key];
+		});
+		var result = window.renderer.render();
+		$("a.download").attr('href',result);
+
+		var encoded=JSON.stringify(dataset);
+		encoded = btoa(encoded);
+		var url = "?class="+encoded;
+		$("a.save").attr('href',url);
+
+		window.kickingoff=false;
+	}
+		
+	//we have everything we need so lets fill out the dropdowns first.
+	var groups=Object.keys(window.renderer.skills);
+	groups.forEach(function(elem){
+		$("div.SKILL select.SKILLGROUP").append("<option value=\""+elem+"\">"+elem+"</option>");
+	});
+	
+	//time to set up the default view
+	$("div.SKILL select.SKILL").hide();
+		
+	//now the generic logic to fill the second dropdown based on the first one.
+	$("div.SKILL select.SKILLGROUP").on('change',function(){
+		var val = $(this).val();
+		$("select.SKILL",$(this).parent()).empty();
+		if(val != "null") {
+			var obj = window.renderer.skills[val];
+			var keys = Object.keys(obj);
+			$("select.SKILL",$(this).parent()).append("<option value=\"null\">Choose</option>");
+			keys.forEach((function(elem){
+				$("select.SKILL",$(this).parent()).append("<option value=\""+elem+"\">"+elem+"</option>");
+			}).bind(this));
+			$("select.SKILL",$(this).parent()).show();
+		} else {
+			$("select.SKILL",$(this).parent()).hide();
+		}
+	});
+	
+	//now the logic to show and hide the corresponding sliders.
+	$("div.SKILL select.SKILL").on('change',function(){
+		//i'm not sure if i need that anymore...
+	});
+	//next up: we need a validation step, which should fire after all changes.
+	//if validation succeeds we need the point calculator and afterwards a rendering step.
+	//so... time for some business logic!
+	$("input, select").on('change',function(){
+		kickoff();
+	});
+	$("select.template").on('change',function(){
+		var template = $("select.template").val();
+		if(template == 'mech') {
+			//mechs have one slot less.
+			$("div.skillset[data-idx=5]").hide();
+		} else {
+			$("div.skillset[data-idx=5]").show();
+		}
+	});
+	//finally: when everything is loaded, one kickoff to get everything in sync.
+	if(renderer.fontLoaded == true) {
+		kickoff(true);
+	}
+	$("#canvas").on('fontLoaded',function(){
+		kickoff(true);
+	});
+
+});
